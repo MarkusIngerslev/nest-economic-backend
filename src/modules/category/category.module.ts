b33896833819d635265a7905eb1670552f5ef6bb
MarkusIngerslev <markusolsen2004@gmail.com>
2025-05-18 15:42:40 +0200

    Imported the use of the user entity in the category controller and service

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
 import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
+import { User } from '../user/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [TypeOrmModule.forFeature([Category, User])],
   providers: [CategoryService],
   controllers: [CategoryController],
   exports: [CategoryService],
